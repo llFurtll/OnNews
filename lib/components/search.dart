080ec9a08af470ac5c698d99e7cd29cfef6f2264
Daniel Melonari <daniel_melonari@hotmail.com>
2020-07-16 00:43:48 -0300

    Continuacao da implementacao do input pesquisar

--- lib/components/search.dart
+++ lib/components/search.dart
@@ -8,7 +8,8 @@ class BuildSearch extends StatefulWidget {
   BuildSearch({this.noticias, this.editingController});
 
   @override
-  BuildSearchState createState() => BuildSearchState(noticias: noticias, editingController: editingController);
+  BuildSearchState createState() => BuildSearchState(
+      noticias: noticias, editingController: editingController);
 }
 
 class BuildSearchState extends State<BuildSearch> {
@@ -17,25 +18,42 @@ class BuildSearchState extends State<BuildSearch> {
 
   BuildSearchState({this.noticias, this.editingController});
 
-  filtrarNoticias(String query) {
-    return "";
+  filtrarNoticias(String query, Future<List<Noticia>> listaNoticia) {
+    Future<List<Noticia>> noticiasSalvas = listaNoticia;
+    if (query.isNotEmpty) {
+      Future<List<Noticia>> noticiasFiltradas = listaNoticia;
+      listaNoticia.then((noticias) {
+        noticias.forEach((elementoNoticia) {
+          if (elementoNoticia.title.contains(query)) {
+            setState(() {
+              noticiasFiltradas.then((value) {
+                value.clear();
+                value.add(elementoNoticia);
+              });
+              listaNoticia = noticiasFiltradas;
+            });
+          }
+        });
+      });
+      return;
+    }
   }
 
-buildSearch(TextEditingController editingController) {
-  return TextField(
-    onChanged: (value) {
-    },
-    controller: editingController,
-    decoration: InputDecoration(
-      labelText: "Pesquisar",
-      hintText: "Pesquisar",
-      prefixIcon: Icon(Icons.search),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(25.0))
+  buildSearch(TextEditingController editingController) {
+    return TextField(
+      onChanged: (value) {
+        filtrarNoticias(value, noticias);
+      },
+      controller: editingController,
+      decoration: InputDecoration(
+        labelText: "Pesquisar",
+        hintText: "Pesquisar",
+        prefixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0))),
       ),
-    ),
-  );
-}
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
